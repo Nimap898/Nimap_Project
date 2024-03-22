16cff9186615a3fd395fec53cad063f850500c02
Nimap898 <163825944+Nimap898@users.noreply.github.com>
2024-03-22 14:18:39 +0530

    Add files via upload

---
+++ project/Jwt/LoginController.cs
@@ -0,0 +1,73 @@
+﻿using JwtExample.Models;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+using System.Web.Http;
+
+namespace JwtExample.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Authenticate(LoginRequest login)
+        {
+            if (login == null)
+            {
+                return BadRequest("Invalid login request.");
+            }
+
+            string username = login.Username.ToLower();
+            string password = login.Password;
+
+            bool isUsernamePasswordValid = (password == "admin");
+
+            if (isUsernamePasswordValid)
+            {
+                string token = CreateToken(username);
+                return Ok(token);
+            }
+            else
+            {
+                HttpResponseMessage responseMsg = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+                return ResponseMessage(responseMsg);
+            }
+        }
+
+        private string CreateToken(string username)
+        {
+            // Set issued at date
+            DateTime issuedAt = DateTime.UtcNow;
+
+            // Set the time when it expires
+            DateTime expires = DateTime.UtcNow.AddMinutes(10);
+
+            // Load secret key from configuration
+            string secretKey = "hgshgdhfghdgsgfgdkjkajsfhagd";
+
+            var securityKey = new SymmetricSecurityKey(Encoding.Default.GetBytes(secretKey));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            var token = new JwtSecurityToken(
+                issuer: "https://localhost:44380",
+                audience: "https://localhost:44380",
+                claims: new[] { new Claim(ClaimTypes.Name, username) },
+                notBefore: issuedAt,
+                expires: expires,
+                signingCredentials: signingCredentials
+            );
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return tokenString;
+        }
+
+    }
+}
---
+++ project/Jwt/TokenValidationHandler.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Web;
+using System.Text;
+using System.Security.Claims;
+
+
+
+namespace JwtExample
+{
+    public class TokenValidationHandler : DelegatingHandler
+    {
+        private static bool TryRetrieveToken(HttpRequestMessage request, out string token)
+        {
+            // Initialize token variable
+            token = null;
+
+            // Declare a variable to store the Authorization header values
+            IEnumerable<string> authzHeaders;
+
+            // Attempt to retrieve the Authorization header values from the request
+            if (!request.Headers.TryGetValues("Authorization", out authzHeaders) || authzHeaders.Count() > 1)
+            {
+                // If the Authorization header does not exist or contains more than one value, return false
+                return false;
+            }
+
+            // Retrieve the first value of the Authorization header
+            var bearerToken = authzHeaders.ElementAt(0);
+
+            // Check if the bearerToken starts with "Bearer "
+            token = bearerToken.StartsWith("Bearer ") ? bearerToken.Substring(7) : bearerToken;
+
+            // Return true to indicate that the token retrieval was successful
+            return true;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            HttpStatusCode statusCode;
+            string token;
+            //determine whether a jwt exists or not
+            if (!TryRetrieveToken(request, out token))
+            {
+                statusCode = HttpStatusCode.Unauthorized;
+                //allow requests with no token - whether a action method needs an authentication can be set with the claimsauthorization attribute
+                return base.SendAsync(request, cancellationToken);
+            }
+
+            try
+            {
+                const string sec = "401b09eab3c013d4ca54922bb802bec8fd5318192b0a75f201d8b3727429090fb337591abd3e44453b954555b7a0812e1081c39b740293f765eae731f5a65ed1";
+                var now = DateTime.UtcNow;
+                var securityKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.Default.GetBytes(sec));
+
+
+                SecurityToken securityToken;
+                JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+                TokenValidationParameters validationParameters = new TokenValidationParameters()
+                {
+                    ValidAudience = "http://localhost:50191",
+                    ValidIssuer = "http://localhost:50191",
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    LifetimeValidator = this.LifetimeValidator,
+                    IssuerSigningKey = securityKey
+                };
+                //extract and assign the user of the jwt
+                Thread.CurrentPrincipal = handler.ValidateToken(token, validationParameters, out securityToken);
+                HttpContext.Current.User = handler.ValidateToken(token, validationParameters, out securityToken);
+
+                return base.SendAsync(request, cancellationToken);
+            }
+            catch (SecurityTokenValidationException e)
+            {
+                statusCode = HttpStatusCode.Unauthorized;
+            }
+            catch (Exception ex)
+            {
+                statusCode = HttpStatusCode.InternalServerError;
+            }
+
+
+            return Task<HttpResponseMessage>.Factory.StartNew(() => new HttpResponseMessage(statusCode) { });
+        }
+
+        public bool LifetimeValidator(DateTime? notBefore, DateTime? expires, SecurityToken securityToken, TokenValidationParameters validationParameters)
+        {
+            if (expires != null)
+            {
+                if (DateTime.UtcNow < expires) return true;
+            }
+            return false;
+        }
+
+    }
+
+}
