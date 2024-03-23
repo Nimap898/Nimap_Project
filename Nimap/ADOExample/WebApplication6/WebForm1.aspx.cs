1c1c0a4573d51a6660059a46f0c28a90b574061c
Nimap898 <163825944+Nimap898@users.noreply.github.com>
2024-03-23 15:35:10 +0530

    Add files via upload

---
+++ Nimap/ADOExample/WebApplication6/WebForm1.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication6
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (
+                 // Creating Connection  
+                 SqlConnection con = new SqlConnection("data source=(LocalDb)\\MSSQLLocalDB; database=aspnet-MVCPractice-20240320034311; integrated security=SSPI")
+             )
+            {
+                SqlCommand cmd = new SqlCommand("select * from products",con);
+                con.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+                con.Close();
+            }
+        }
+    }
+}
